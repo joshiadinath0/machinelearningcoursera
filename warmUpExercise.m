05cc72973260047c903be396f5e0e036e940e2ef
Adinath Joshi <72252165+joshiadinath0@users.noreply.github.com>
2021-03-28 12:54:18 +0530

    Add files via upload

---
+++ warmUpExercise.m
@@ -0,0 +1,21 @@
+function A = warmUpExercise()
+%WARMUPEXERCISE Example function in octave
+%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = [];
+% ============= YOUR CODE HERE ==============
+% Instructions: Return the 5x5 identity matrix 
+%               represent the return values (at the top of the file)
+%               In octave, we return values by defining which variables
+%               and then set them accordingly. 
+
+A=eye(5);
+
+
+
+
+
+% ===========================================
+
+
+end
